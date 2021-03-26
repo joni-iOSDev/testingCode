d6db7abade89d94d050a6a2f65d2068010067305
Jonatan Gonzalez <jonatan.gonzalez@mercadolibre.com>
2021-03-26 20:06:30 -0300

    Initial Commit

---
+++ AppTestingCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppTestingCode
+//
+//  Created by Jonatan Ariel Marcos Gonzalez on 26/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AppTestingCodeTests/AppTestingCodeTests.swift
@@ -0,0 +1,33 @@
+//
+//  AppTestingCodeTests.swift
+//  AppTestingCodeTests
+//
+//  Created by Jonatan Ariel Marcos Gonzalez on 26/03/2021.
+//
+
+import XCTest
+@testable import AppTestingCode
+
+class AppTestingCodeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
